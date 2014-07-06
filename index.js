3babf88f655a287c74031f4c4a962f3a3cfcae2b
Kim Røen <kim@kimroen.com>
2014-07-06 15:55:19 +0200

    A new hope

--- index.js
+++ index.js
@@ -4,31 +4,24 @@ var path = require('path');
 var fs = require('fs');
 var autoprefixer = require('broccoli-autoprefixer');
 
-function AutoprefixerPlugin(options) {
-  this.name = 'ember-cli-autoprefixer';
-  this.ext = 'css';
-  this.options = options || {};
-}
-
-AutoprefixerPlugin.prototype.toTree = function(tree) {
-  return autoprefixer(tree, this.options);
-};
-
 function EmberCLIAutoprefixer(project) {
   this.project = project;
   this.name = 'Ember CLI Autoprefixer';
 }
 
-EmberCLIAutoprefixer.prototype.treeFor = function treeFor() {
+EmberCLIAutoprefixer.prototype.postprocessTree = function (type, tree) {
+  if (type === 'all' || type === 'styles') {
+    tree = autoprefixer(tree, this.options);
+  }
+
+  return tree;
 };
 
 EmberCLIAutoprefixer.prototype.included = function included(app) {
-  var registry = app.registry;
   this.app = app;
-
-  var plugin = new AutoprefixerPlugin(this.app.options.autoprefixerOptions);
-
-  registry.add('css', plugin);
+  this.options = this.app.options.autoprefixer;
 };
+
+EmberCLIAutoprefixer.prototype.treeFor = function treeFor() {};
 
 module.exports = EmberCLIAutoprefixer;
