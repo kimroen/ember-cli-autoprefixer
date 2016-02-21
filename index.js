706384a8acd755e5768449afa215e746bd322874
Kim Røen <kim@kimroen.com>
2016-02-21 14:24:39 +0100

    Update lodash dependency

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 'use strict';
 
 var autoprefixer = require('broccoli-autoprefixer');
-var defaults     = require('lodash').defaults;
+var defaults     = require('lodash/defaults');
 
 module.exports = {
   name: 'ember-cli-autoprefixer',
   included: function(app, parentAddon) {
     this.app = app;
-    
+
     if (typeof app.import !== 'function' && app.app) {
       this.app = app = app.app;
     }
-    
+
     this._super.included.apply(this, arguments);
     this.options = defaults(this.app.options.autoprefixer || {}, {
       enabled: true
