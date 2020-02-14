29aac531b4615d6f69481be9669fd7d01ff0509f
Martin Feckie <mfeckie@gmail.com>
2020-02-14 15:54:21 +0800

    Add Ember 3.8 to ember-try matrix

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,6 +6,14 @@ module.exports = async function() {
   return {
     scenarios: [
       {
+        name: 'ember-lts-3.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.8.0'
+          }
+        }
+      },
+      {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
